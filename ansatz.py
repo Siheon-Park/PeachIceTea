a8c534fa7a77c86c019c29769597ca22e51b0cd7
Ju-Young Ryu <72022558+jyryu98@users.noreply.github.com>
2022-06-27 21:11:35 +0900

    Add files via upload
    Changed implementation of ansatzes

--- ansatz.py
+++ ansatz.py
@@ -5,63 +5,29 @@ Convolution & Pooling ansatzes used in the reference paper:
 Hur, Tak, Leeseok Kim, and Daniel K. Park. "Quantum convolutional neural network for classical data classification." 
 Quantum Machine Intelligence 4.1 (2022): 1-18.
 
-To get the dimension of input weights, use the ".ndim_params" attribute to get this value.
-For example:
-
-nparams = ConvCirc1(weights = [0,1], wires = [0, 1]).ndim_params
-
-will apply ConvCirc1 AND nparams will be equal to 2
+Make an instance of each ansatz. One can get the number of trainable parameters using the ".num_params" attribute.
+Use the ".apply" method to apply the ansatz using the appropriate weights / qubits.
 """
 
 # Convolutional Layer Ansatz
-class ConvCirc1(qml.operation.Operation):
+class ConvCirc1():
     
-    num_wires = qml.operation.AnyWires
-    grad_method = None
-    
-    def __init__(self, weights = None, wires = None, do_queue=True, id=None):
-        super().__init__(weights, wires=wires, do_queue=do_queue, id=id)
+    def __init__(self) -> None:
+        self.num_params = 2
 
-    @property
-    def ndim_params(self):
-        return 2
-
-    @staticmethod
-    def compute_decomposition(weights, wires):
-        decomp = []
-        decomp.append(qml.RY(weights[0], wires = wires[0]))
-        decomp.append(qml.RY(weights[1], wires = wires[1]))
-        decomp.append(qml.CNOT(wires = wires))
-        return decomp
+    def apply(self, weights, wires):
+        qml.RY(weights[0], wires = wires[0])
+        qml.RY(weights[1], wires = wires[1])
+        qml.CNOT(wires = wires)
 
 # Pooling Layer Ansatz
-class PoolingCirc(qml.operation.Operation):
+class PoolingCirc():
 
-    num_wires = qml.operation.AnyWires
-    grad_method = None
+    def __init__(self) -> None:
+        self.num_params = 2
     
-    def __init__(self, weights = None, wires = None, do_queue=True, id=None):
-        super().__init__(weights, wires=wires, do_queue=do_queue, id=id)
-
-    @property
-    def ndim_params(self):
-        return 2
-
-    @staticmethod
-    def compute_decomposition(weights, wires):
-        decomp = []
-        decomp.append(qml.CRZ(weights[0], wires = wires))
-        decomp.append(qml.X(wires = wires[0]))
-        decomp.append(qml.CRX(weights[1], wires = wires))
-        decomp.append(qml.X(wires = wires[0]))
-        return decomp
-
-if __name__=='__main__':
-    dev = qml.device('default.qubit', wires=2)
-
-    @qml.qnode(dev)
-    def circuit(w):
-        ConvCirc1(w, wires=(0, 1))
-        return qml.PauliZ(0)
-
-    print(qml.draw(circuit)([0, 1]))
+    def apply(self, weights, wires):
+        qml.CRY(weights[0], wires = wires)
+        qml.X(wires[0])
+        qml.CRY(weights[1], wires = wires)
+        qml.X(wires[0])
