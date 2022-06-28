d01694087e846dd997fb8d7d923e75bb7d1959da
Siheon Park <sihoney97@kaist.ac.kr>
2022-06-28 14:47:41 +0900

    add loading

--- datasetloading.py
+++ datasetloading.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+from sympy import im
 
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import Dataset
@@ -43,36 +44,6 @@ class DataframeDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-def preprocess_train_and_test_dataset(path_to_train, path_to_test, pca_dim:int=5, save:bool=False, dummy:bool=False):
-    training_data = read_csv(path_to_train)
-    test_data = read_csv(path_to_test)
-
-    if not dummy:
-        training_label = training_data.iloc[:,0]
-        training_data = training_data.iloc[:,1:]
-        test_label = test_data.iloc[:,0]
-        test_data = test_data.iloc[:,1:]
-
-        scaler = StandardScaler()
-        training_data = scaler.fit_transform(training_data)
-        test_data = scaler.transform(test_data)
-
-        pca = PCA(n_components=pca_dim)
-        training_data = pca.fit_transform(training_data)
-        test_data = pca.transform(test_data)
-
-        training_data = pd.DataFrame(training_data)
-        training_data.insert(0, 'label', training_label)
-        if save:
-            training_data.to_csv("training_data.csv", index = False)
-
-        training_data = pd.DataFrame(test_data)
-        training_data.insert(0, 'label', test_label)
-        if save:
-            training_data.to_csv("test_data.csv", index = False)
-
-    return DataframeDataset(training_data), DataframeDataset(training_data)
-
 class TrainTestLoader(object):
     def __init__(self, path_to_train, path_to_test):
             self.training_data = read_csv(path_to_train)
@@ -108,3 +79,34 @@ class TrainTestLoader(object):
     def save(self):
         self.training_data.to_csv("./training_data.csv", index = False)
         self.test_data.to_csv("./test_data.csv", index = False)
+
+    def load(self):
+        self.training_data = read_csv("./training_data.csv")
+        self.test_data = read_csv("./test_data.csv")        
+
+
+if __name__=="__main__":
+    TRAINSIZE =10000
+    VALSIZE=1000
+    TESTSIZE=1000
+    SEED=1234
+    from torch.utils.data import random_split
+    loader = TrainTestLoader('./mnist_train.csv', './mnist_test.csv')
+    loader.scale_pca(n_dim=30)
+    training_set, test_set = loader.return_dataset()
+    training_set.filter([0, 1])
+    training_set, validation_set = random_split(training_set, [TRAINSIZE, len(training_set)-TRAINSIZE], generator=torch.Generator().manual_seed(SEED))
+    validation_set, _ = random_split(validation_set, [VALSIZE, len(validation_set)-VALSIZE], generator=torch.Generator().manual_seed(SEED))
+    test_set, _ = random_split(test_set, [TESTSIZE, len(test_set)-TESTSIZE], generator=torch.Generator().manual_seed(SEED))
+
+    # Create data loaders for our datasets; shuffle for training, not for test
+    training_loader = DataLoader(training_set, batch_size=25, shuffle=True, generator=torch.Generator().manual_seed(SEED))
+    validation_loader = DataLoader(validation_set, batch_size=25, shuffle=False, generator=torch.Generator().manual_seed(SEED))
+    test_loader = DataLoader(test_set, batch_size=25, shuffle=False, generator=torch.Generator().manual_seed(SEED))
+
+    # ====================================
+
+    # Report split sizes
+    print('Training set has {} instances'.format(len(training_set)))
+    print('validation set has {} instances'.format(len(validation_set)))
+    print('test set has {} instances'.format(len(test_set)))
